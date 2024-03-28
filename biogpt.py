1593f56420a6b490893cb2e837966c1548049d10
MarinaLex1711 <128158641+MarinaLex1711@users.noreply.github.com>
2024-03-28 12:00:32 +0100

    Update biogpt.py
    add cache_data

--- biogpt.py
+++ biogpt.py
@@ -83,7 +83,7 @@ st.markdown(
 
 
 # Laden des Modells und der Tokenizer
-@st.cache(allow_output_mutation=True)
+@st.cache_data(allow_output_mutation=True)
 def load_model():
     model = BioGptForCausalLM.from_pretrained("microsoft/biogpt")
     tokenizer = BioGptTokenizer.from_pretrained("microsoft/biogpt")
